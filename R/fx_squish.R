b990c3735967734a5823aee599bc6f9f75ed25ed
Jacquie Tran <jacquie.tran@gmail.com>
2022-04-09 16:43:01 +1200

    Functionalising some sturff

---
+++ R/fx_squish.R
@@ -0,0 +1,35 @@
+squish <- function(seed_num){
+  
+  # Requires {tibble}
+  
+  set.seed(seed_num)
+  seed_vec <- sample(seq(1, 1000000, by = 1), 8, replace = FALSE)
+  
+  # Select x coords for control points
+  set.seed(seed_vec[1])
+  x1 <- runif(1, min = -0.2, max = 0.45)
+  set.seed(seed_vec[2])
+  x2 <- runif(1, min = -0.2, max = 0.45)
+  set.seed(seed_vec[3])
+  x3 <- runif(1, min = 0.55, max = 1.2)
+  set.seed(seed_vec[4])
+  x4 <- runif(1, min = 0.55, max = 1.2)
+  
+  # Select y coords for control points
+  set.seed(seed_vec[5])
+  y1 <- runif(1, min = -0.2, max = 0.45)
+  set.seed(seed_vec[6])
+  y2 <- runif(1, min = 0.55, max = 1.2)
+  set.seed(seed_vec[7])
+  y3 <- runif(1, min = 0.55, max = 1.2)
+  set.seed(seed_vec[8])
+  y4 <- runif(1, min = -0.2, max = 0.45)
+  
+  set.seed(seed_num)
+  squishy_blob <- tibble::tibble(
+    x = c(x1, x2, x3, x4),
+    y = c(y1, y2, y3, y4))
+  
+  return(squishy_blob)
+  
+}
---
+++ R/fx_throw_shapes2.R
@@ -0,0 +1,36 @@
+throw_shapes2 <- function(seed_num, n_shapes, palette){
+  
+  # Requires {purrr}, {dplyr}
+  
+  set.seed(seed_num)
+  seed_vec <- sample(seq(1, 10000000, by = 1), n_shapes, replace = FALSE)
+  
+  purrr::map_df(seed_vec, function(i){
+    
+    set.seed(i)
+    n_control_points <- sample(seq(3, 6, by = 1), 1)
+    
+    set.seed(i)
+    data <- data.frame(
+      seed_num = i,
+      x = scales::rescale(runif(n_control_points), to = c(-0.2, 1.2)),
+      y = scales::rescale(runif(n_control_points), to = c(-0.2, 1.2)))
+    
+  }) -> splines_data
+  
+  set.seed(seed_num)
+  splines_with_colours <- splines_data |>
+    dplyr::distinct(seed_num) |>
+    dplyr::mutate(
+      hex_fill = sample(palette, dplyr::n(), replace = TRUE),
+      spline_size = sample(
+        c(seq(0.05, 0.4, by = 0.05), c(0.6, 0.7, 0.8, 0.9)),
+        n(), replace = TRUE,
+        prob = c(rep(0.1, times = 8), rep(0.05, times = 4))))
+  
+  splines_for_plot <- dplyr::left_join(
+    splines_data, splines_with_colours, by = "seed_num")
+  
+  return(splines_for_plot)
+  
+}
